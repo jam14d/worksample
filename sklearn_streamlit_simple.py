7b1ea34593a55a68ac08b84ac4735b3c864476c0
jmortelrb <jmortel@revealbio.com>
2024-03-07 14:03:23 -0800

    fun markdown and titles and sidebars

--- sklearn_streamlit_simple.py
+++ sklearn_streamlit_simple.py
@@ -9,8 +9,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, precision_recall_curve
 from sklearn.metrics import precision_score, recall_score 
 
+"Building a Machine Learning Web APp with Streamlit and Python - In Progress"
+
 def main():
     st.title("Binary Classification Web App")
+    st.sidebar.title("Binary Classification Web App")
+    st.markdown("Are your mushrooms edible or poisonous? 🍄")
+    st.sidebar.markdown("Are your mushrooms edible or poisonous? 🍄")
 
 
 
