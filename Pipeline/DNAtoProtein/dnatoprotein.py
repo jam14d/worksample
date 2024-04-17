06364c52b3b9bf64387aa375b3b3cbf760102e31
jmortelrb <jmortel@revealbio.com>
2024-04-16 17:12:52 -0700

    dnatoprotein

--- Pipeline/DNAtoProtein/dnatoprotein.py
+++ Pipeline/DNAtoProtein/dnatoprotein.py
@@ -30,6 +30,35 @@ def highlight_stop_codons(rna_sequence):
         rna_sequence = rna_sequence.replace(codon, f"<span style='color:red; font-weight:bold;'>{codon}</span>")
     return rna_sequence
 
+def translate_rna_to_protein(rna_sequence):
+    """Translates RNA sequence into protein based on codon mapping."""
+    codon_to_amino_acid = {
+        'UUU': 'Phe', 'UUC': 'Phe', 'UUA': 'Leu', 'UUG': 'Leu',
+        'UCU': 'Ser', 'UCC': 'Ser', 'UCA': 'Ser', 'UCG': 'Ser',
+        'UAU': 'Tyr', 'UAC': 'Tyr', 'UAA': 'Stop', 'UAG': 'Stop',
+        'UGU': 'Cys', 'UGC': 'Cys', 'UGA': 'Stop', 'UGG': 'Trp',
+        'CUU': 'Leu', 'CUC': 'Leu', 'CUA': 'Leu', 'CUG': 'Leu',
+        'CCU': 'Pro', 'CCC': 'Pro', 'CCA': 'Pro', 'CCG': 'Pro',
+        'CAU': 'His', 'CAC': 'His', 'CAA': 'Gln', 'CAG': 'Gln',
+        'CGU': 'Arg', 'CGC': 'Arg', 'CGA': 'Arg', 'CGG': 'Arg',
+        'AUU': 'Ile', 'AUC': 'Ile', 'AUA': 'Ile', 'AUG': 'Met',
+        'ACU': 'Thr', 'ACC': 'Thr', 'ACA': 'Thr', 'ACG': 'Thr',
+        'AAU': 'Asn', 'AAC': 'Asn', 'AAA': 'Lys', 'AAG': 'Lys',
+        'AGU': 'Ser', 'AGC': 'Ser', 'AGA': 'Arg', 'AGG': 'Arg',
+        'GUU': 'Val', 'GUC': 'Val', 'GUA': 'Val', 'GUG': 'Val',
+        'GCU': 'Ala', 'GCC': 'Ala', 'GCA': 'Ala', 'GCG': 'Ala',
+        'GAU': 'Asp', 'GAC': 'Asp', 'GAA': 'Glu', 'GAG': 'Glu',
+        'GGU': 'Gly', 'GGC': 'Gly', 'GGA': 'Gly', 'GGG': 'Gly'
+    }
+    protein = []
+    for i in range(0, len(rna_sequence), 3):
+        codon = rna_sequence[i:i+3]
+        if codon in codon_to_amino_acid and codon_to_amino_acid[codon] != 'Stop':
+            protein.append(codon_to_amino_acid[codon])
+        elif codon in codon_to_amino_acid and codon_to_amino_acid[codon] == 'Stop':
+            break
+    return ' '.join(protein)
+
 def run_pipeline(input_string, mutation_rate=0):
     """
     Runs the transformation pipeline on an input string and applies mutations if specified.
@@ -50,7 +79,7 @@ def run_pipeline(input_string, mutation_rate=0):
     return original_dna_output, mutated_dna_output
 
 # Streamlit interface
-st.title('DNA and RNA Transcription Simulator')
+st.title('DNA to Protein Simulator')
 user_input = st.text_area("Enter your text to convert into DNA:", "Type your text here...")
 mutation_rate = st.slider("Mutation rate (in percentage):", min_value=0.0, max_value=100.0, value=0.0, step=0.1) / 100
 
@@ -69,6 +98,11 @@ if st.button('Transcribe'):
         st.text("Resulting RNA Sequence:")
         highlighted_rna = highlight_stop_codons(rna_output)
         st.markdown(highlighted_rna, unsafe_allow_html=True)
+
+        # Translate RNA to protein
+        protein_sequence = translate_rna_to_protein(rna_output)
+        st.text("Translated Protein Sequence:")
+        st.write(protein_sequence)
     else:
         st.error("Please enter some text to process.")
 
