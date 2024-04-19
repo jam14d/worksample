ef575c2ebdc6c6544f13a30a6d5cb4cda1ae7f45
jmortelrb <jmortel@revealbio.com>
2024-04-19 14:50:58 -0700

    protein synthesis modularity

--- CodetoCodons/dnatoprotein.py
+++ CodetoCodons/dnatoprotein.py
@@ -8,6 +8,8 @@ from dna_base_converter import DNABaseConverter
 from space_remover import SpaceRemover
 from special_characters_remover import SpecialCharactersRemover
 from amino_acid_data import amino_acid_properties
+from protein_synthesis import translate_rna_to_protein
+
 
 
 def mutate_dna(dna_sequence, mutation_rate):
@@ -31,36 +33,6 @@ def highlight_stop_codons(rna_sequence):
     for codon in stop_codons:
         rna_sequence = rna_sequence.replace(codon, f"<span style='color:red; font-weight:bold;'>{codon}</span>")
     return rna_sequence
-
-def translate_rna_to_protein(rna_sequence):
-    """Translates RNA sequence into protein based on codon mapping."""
-    codon_to_amino_acid = {
-        'UUU': 'Phe', 'UUC': 'Phe', 'UUA': 'Leu', 'UUG': 'Leu',
-        'UCU': 'Ser', 'UCC': 'Ser', 'UCA': 'Ser', 'UCG': 'Ser',
-        'UAU': 'Tyr', 'UAC': 'Tyr', 'UAA': 'Stop', 'UAG': 'Stop',
-        'UGU': 'Cys', 'UGC': 'Cys', 'UGA': 'Stop', 'UGG': 'Trp',
-        'CUU': 'Leu', 'CUC': 'Leu', 'CUA': 'Leu', 'CUG': 'Leu',
-        'CCU': 'Pro', 'CCC': 'Pro', 'CCA': 'Pro', 'CCG': 'Pro',
-        'CAU': 'His', 'CAC': 'His', 'CAA': 'Gln', 'CAG': 'Gln',
-        'CGU': 'Arg', 'CGC': 'Arg', 'CGA': 'Arg', 'CGG': 'Arg',
-        'AUU': 'Ile', 'AUC': 'Ile', 'AUA': 'Ile', 'AUG': 'Met',
-        'ACU': 'Thr', 'ACC': 'Thr', 'ACA': 'Thr', 'ACG': 'Thr',
-        'AAU': 'Asn', 'AAC': 'Asn', 'AAA': 'Lys', 'AAG': 'Lys',
-        'AGU': 'Ser', 'AGC': 'Ser', 'AGA': 'Arg', 'AGG': 'Arg',
-        'GUU': 'Val', 'GUC': 'Val', 'GUA': 'Val', 'GUG': 'Val',
-        'GCU': 'Ala', 'GCC': 'Ala', 'GCA': 'Ala', 'GCG': 'Ala',
-        'GAU': 'Asp', 'GAC': 'Asp', 'GAA': 'Glu', 'GAG': 'Glu',
-        'GGU': 'Gly', 'GGC': 'Gly', 'GGA': 'Gly', 'GGG': 'Gly'
-    }
-    protein = []
-    for i in range(0, len(rna_sequence), 3):
-        codon = rna_sequence[i:i+3]
-        if codon in codon_to_amino_acid and codon_to_amino_acid[codon] != 'Stop':
-            protein.append(codon_to_amino_acid[codon])
-        elif codon in codon_to_amino_acid and codon_to_amino_acid[codon] == 'Stop':
-            break
-    return ' '.join(protein)
-
 
 def run_pipeline(input_string, mutation_rate=0):
     """
