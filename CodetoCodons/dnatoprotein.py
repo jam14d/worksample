22e8148f61cb9fab52f47d897559f4fa2bfa1699
jmortelrb <jmortel@revealbio.com>
2024-04-19 15:09:28 -0700

    prepend atg

--- CodetoCodons/dnatoprotein.py
+++ CodetoCodons/dnatoprotein.py
@@ -7,10 +7,7 @@ from character_capitalizer import CharacterCapitalizer
 from dna_base_converter import DNABaseConverter
 from space_remover import SpaceRemover
 from special_characters_remover import SpecialCharactersRemover
-from amino_acid_data import amino_acid_properties
 from protein_synthesis import translate_rna_to_protein
-
-
 
 def mutate_dna(dna_sequence, mutation_rate):
     """Mutates the given DNA sequence based on the mutation rate."""
@@ -34,9 +31,10 @@ def highlight_stop_codons(rna_sequence):
         rna_sequence = rna_sequence.replace(codon, f"<span style='color:red; font-weight:bold;'>{codon}</span>")
     return rna_sequence
 
-def run_pipeline(input_string, mutation_rate=0):
+def run_pipeline(input_string, mutation_rate=0, prepend_start_codon=False):
     """
     Runs the transformation pipeline on an input string and applies mutations if specified.
+    Optionally prepends 'ATG' to simulate a start codon.
     """
     pipeline = Pipeline()
     pipeline.add(StringReader())
@@ -44,12 +42,12 @@ def run_pipeline(input_string, mutation_rate=0):
     pipeline.add(DNABaseConverter())
     pipeline.add(SpaceRemover())
     pipeline.add(SpecialCharactersRemover())
-    
+
     original_dna_output = pipeline.execute(input_string)
-    mutated_dna_output = original_dna_output
-    
-    if mutation_rate > 0:
-        mutated_dna_output = mutate_dna(original_dna_output, mutation_rate)
+    if prepend_start_codon:
+        original_dna_output = 'ATG' + original_dna_output
+
+    mutated_dna_output = mutate_dna(original_dna_output, mutation_rate)
     
     return original_dna_output, mutated_dna_output
 
@@ -57,11 +55,12 @@ def run_pipeline(input_string, mutation_rate=0):
 st.title('DNA to Protein Simulator')
 user_input = st.text_area("Enter your text to convert into DNA:", "Type your text here...")
 mutation_rate = st.slider("Mutation rate (in percentage):", min_value=0.0, max_value=100.0, value=0.0, step=0.1) / 100
+prepend_start_codon = st.checkbox("Prepend 'ATG' start codon to DNA sequence", value=False)
 
 if st.button('Transcribe and Translate'):
     if user_input:
         # Generate and mutate DNA
-        original_dna, mutated_dna = run_pipeline(user_input, mutation_rate)
+        original_dna, mutated_dna = run_pipeline(user_input, mutation_rate, prepend_start_codon)
         st.text("Original DNA Sequence (before mutation):")
         st.write(original_dna)
 
@@ -82,5 +81,5 @@ if st.button('Transcribe and Translate'):
         st.error("Please enter some text to process.")
 
 # Display the codon table image
-codon_table_image_url = "https://www.researchgate.net/profile/Anders-Esberg/publication/267702580/figure/fig2/AS:661826920513537@1534803242980/The-codon-table-The-genetic-code-is-composed-of-four-different-letters-U-C-A-and-G.png"  # Change this URL to the path where your image is hosted
-st.image(codon_table_image_url, caption="Codon Table", use_column_width=True)
+#codon_table_image_url = "https://www.researchgate.net/profile/Anders-Esberg/publication/267702580/figure/fig2/AS:661826920513537@1534803242980/The-codon-table-The-genetic-code-is-composed-of-four-different-letters-U-C-A-and-G.png"
+#st.image(codon_table_image_url, caption="Codon Table", use_column_width=True)
