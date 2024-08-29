ebb78a179c7f88237e06cff0bbc49f8d9bce3117
jmortelrb <jmortel@revealbio.com>
2024-08-29 14:43:08 -0700

    cleaning up

--- Lab_Wizard/app_wizard.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-# Path to your Streamlit script
-streamlit_script = "/Users/jamieannemortel/Projects/Lab_Wizard/StockSolution_Wizard.py"
-
-# Launch the Streamlit app
-subprocess.run(["streamlit", "run", streamlit_script])
