a096b0f6a17b118eb302915fddc6f1a65dc2bcfb
jmortelrb <jmortel@revealbio.com>
2024-09-16 15:59:31 -0700

    moved ml projects to random folder

--- Learning_Stuff/generate_simulated_data.py
+++ Learning_Stuff/generate_simulated_data.py
@@ -1,5 +1,21 @@
 import numpy as np
 
+
+"""Stimulus Data:
+
+The stimulus data in STA refers to the recorded representation of the stimuli. For instance, if you are presenting visual patterns, the stimulus data might be a series of images or a matrix of pixel values corresponding to each time point.
+Alignment and Averaging:
+
+To perform STA, you extract the segments of this stimulus data that occur just before each recorded spike. These segments are then averaged to find out what specific features of the stimulus are associated with the neuron’s spikes.
+
+
+Example to Illustrate
+Stimulus Presentation: Suppose you show a sequence of images to a subject and record the times when the subject’s brain activity spikes.
+Stimulus Data: The actual images or their numerical representation are the stimulus data you record.
+Analysis with STA: For each spike, you look at the images presented just before the spike (e.g., the last 100 ms before the spike) and average these images to determine which visual features are most likely to trigger spikes."""
+
+
+
 # Parameters
 num_stimuli = 1000        # Number of stimuli
 stimulus_length = 50      # Length of each stimulus
