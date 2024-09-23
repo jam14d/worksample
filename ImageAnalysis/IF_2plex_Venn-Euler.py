cb33c57f01bb20553f0dfcf51fd6fcb5fe3653fe
jmortelrb <jmortel@revealbio.com>
2024-09-23 11:04:03 -0700

    appropriate name lol

--- ImageAnalysis/IF_2plex_Venn-Euler.py
+++ ImageAnalysis/IF_2plex_Venn-Euler.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn2, venn2_circles
 
+
+#probably going to change this to use eule 
+
 # Set the paths
 path_det = "/Users/jamieannemortel/Downloads/Andrew/detections"
 path_ano = "/Users/jamieannemortel/Downloads/Andrew/annotations"
